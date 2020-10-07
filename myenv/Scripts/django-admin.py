c103adca8aa19d966670bc066256e2aed7f05dc7
Point <jatupoint@hotmail.com>
2020-10-08 06:03:14 +0700

    jatupon61010122 master

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\#3thirdyear(3)\cloud\clone\djangofullstack\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
